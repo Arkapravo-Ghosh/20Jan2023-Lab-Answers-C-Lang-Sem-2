5a081c0b719007fff1dcb4f3fb28c3041208daf4
Arkapravo Ghosh <65386474+Arkapravo-Ghosh@users.noreply.github.com>
2023-02-05 11:57:49 +0530

    Update Lab2-12.c

--- Lab2-Answers/Lab2-12.c
+++ Lab2-Answers/Lab2-12.c
@@ -3,7 +3,7 @@ int main() {
     double a;
     printf("Enter a number: ");
     scanf("%lf", &a);
-    int b=(int)a*100;
-    printf("The required output is %d.\n",(b%10)+((b/10)%10));
+    int b = (int)a * 100;
+    printf("The required output is %d.\n",(b % 10) + ((b / 10) % 10));
     return 0;
 }
